eea264e9bff1bc6d9a74cc3a0e176947ca404714
Khadizha <k_momunova@kbtu.kz>
2025-01-25 18:46:55 +0500

    commit

---
+++ lab1.5.py
@@ -0,0 +1,32 @@
+x = "Hello World"
+#display x:
+print(x)
+#display the data type of x:
+print(type(x)) 
+
+y = 20
+#display y:
+print(y)
+#display the data type of y:
+print(type(y)) 
+
+z = 20.5
+#display z:
+print(z)
+#display the data type of z:
+print(type(z)) 
+
+q = 1j
+#display q:
+print(q)
+#display the data type of q:
+print(type(q)) 
+
+j = True
+#display j:
+print(j)
+#display the data type of j:
+print(type(j)) 
+
+
+
