6aa3e7dc54b5cf4a980e175f810e9a1a47985f8f
Khadizha <k_momunova@kbtu.kz>
2025-02-07 22:37:10 +0500

    c

--- lab3 pp2/lab3.3.py
+++
@@ -1,8 +0,0 @@
-class rectangle(shape):
-    def init(self, length, width):
-        super().init()
-        self.length = length
-        self.width = width
-
-    def area(self):
-        return self.length * self.width
