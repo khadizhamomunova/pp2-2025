460c688a3ff70f9d7c9ebbae2ba7e643319026fe
Khadizha <k_momunova@kbtu.kz>
2025-04-05 13:41:55 +0500

    commit

--- lab7 pp2/task2lab7.py
+++ lab7 pp2/task2lab7.py
@@ -1,20 +1,58 @@
 import pygame
+import os
+
 pygame.init()
-screen = pygame.display.set_mode((600, 600))
-pygame.display.set_caption("player")
+
+screen_width = 375
+screen_height = 812
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+iphone_img = pygame.image.load('lab7 pp2/phone.jpeg')
+iphone_img = pygame.transform.scale(iphone_img, (screen_width, screen_height))
+
+music_dir = 'lab7 pp2/lab7_player_songs_2MS (128 kbps).mp3'
+
+music_files = [f for f in os.listdir(music_dir) if f.endswith('.mp3')]
+
+current_track = 0
+paused = False
+
+pygame.mixer.music.load(os.path.join(music_dir, music_files[current_track]))
+
+pygame.mixer.music.play()
+
+font = pygame.font.SysFont(None, 30) 
 
 running = True
 while running:
- 
-    screen.fill((255,255,255)) 
-    pygame.draw.line(screen, ("red"), (50, 480), (550, 480), 40)
-    pygame.draw.polygon(screen,("red"),((200,100),(400,250),(200,400)))
-    
-    pygame.display.update()
-    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-            pygame.quit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                if paused:
+                    pygame.mixer.music.unpause()
+                    paused = False
+                else:
+                    pygame.mixer.music.pause()
+                    paused = True
+            elif event.key == pygame.K_RIGHT:
+                current_track = (current_track + 1) % len(music_files)
+                pygame.mixer.music.load(os.path.join(music_dir, music_files[current_track]))
+                pygame.mixer.music.play()
+            elif event.key == pygame.K_LEFT:
+                current_track = (current_track - 1) % len(music_files)
+                pygame.mixer.music.load(os.path.join(music_dir, music_files[current_track]))
+                pygame.mixer.music.play()
 
+    screen.fill((255, 255, 255))
+    screen.blit(iphone_img, (0, 0))
 
+    music_name = music_files[current_track].split('.')[0]
+    text = font.render(music_name, True, (0, 0, 0))
+    text_rect = text.get_rect(center=(screen_width // 2, 510))
+    screen.blit(text, text_rect)
+
+    pygame.display.flip()
+
+pygame.quit()
